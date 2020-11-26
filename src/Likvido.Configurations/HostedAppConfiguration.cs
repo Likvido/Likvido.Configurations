94e1f075fdee09b84a0c232f15ce9bcf2d7e53ab
Alexander Boychenko <boichenko.alexander@gmail.com>
2020-11-26 15:51:04 +0700

    Add initial configurations implementation

---
+++ src/Likvido.Configurations/HostedAppConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Likvido.Configurations
+{
+    public static class HostedAppConfiguration
+    {
+        public static void ApplyHostedAppConfiguration(this IConfigurationBuilder cb, string[]? args = null)
+        {
+            //removing everything besides the very first source `ChainedConfigurationSource`
+            //added by Host.CreateDefaultBuilder
+            while (cb.Sources.Count > 1)
+            {
+                cb.Sources.RemoveAt(1);
+            }
+            cb.ApplyAppConfiguration(args);
+        }
+    }
+}
